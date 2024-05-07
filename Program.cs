b15bea8612bfe12ba5d567a8c9dcbcfd5bc79ac6
Rustam <89173709860xx@gmail.com>
2024-05-07 12:14:13 +0500

    program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿string[] ArrayOf3lessLetters(string[] str)
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    int j = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            result[j] = str[i];
+            j++;
+        }
+    }
+    return result;
+}
+//write function for outtput massive of strings on the screen
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+string[] array = { "hello", "2", "world", ":-)" };
+string[] result = ArrayOf3lessLetters(array);
+PrintArray(result);
